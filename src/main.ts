ae38b5e1664e7eafdc127ace96928f328707acb5
Navid Dezashibi <dezashibi@gmail.com>
2024-02-13 10:49:03 +0400

    force to crash if the 'app' is not found

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import './app.css'
-import App from './App.svelte'
+import './app.css';
+import App from './App.svelte';
 
+// Attempt to get the element
+const appElement = document.getElementById('app');
+
+// Check if the element is not found
+if (!appElement) {
+    throw new Error("Failed to find the element with ID 'app'");
+}
+
+// Since we've thrown an error if appElement is null, TypeScript knows appElement is not null here
 const app = new App({
-  target: document.getElementById('app'),
-})
+    target: appElement,
+});
 
-export default app
+export default app;
