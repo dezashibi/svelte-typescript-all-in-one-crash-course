1f9dff54927b3e8d705cce8c36629e3239ce064f
Navid Dezashibi <dezashibi@gmail.com>
2024-02-13 11:18:44 +0400

    component example

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,15 @@
-<h1>Hello Svelte</h1>
+<script>
+    import Funny from "./Funny.svelte";
+    let customName = "James";
+</script>
+
+<Funny wait={4000} />
+<Funny name={customName} favoriteColorBegin="green" favoriteColorEnd="red" />
+
+<h1>Hello Again!</h1>
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
---
+++ src/Funny.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let name = "Navid";
+    export let favoriteColorBegin = "red";
+    export let favoriteColorEnd = "blue";
+
+    export let wait = 3000;
+
+    setTimeout(function () {
+        favoriteColorBegin = favoriteColorEnd;
+    }, wait);
+</script>
+
+<h1>
+    Hello Svelte Developer,
+    <span style="color: {favoriteColorBegin}">{name}</span> 👏
+</h1>
